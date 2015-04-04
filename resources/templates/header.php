ca13c980533cb2d921360cd85081bf00d299b01c
Matt <matthew.prahl@mymail.champlain.edu>
2015-04-04 17:25:39 -0400

    Small fix for IE compatability

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -3,6 +3,8 @@
 <title><?php echo $pageTitle ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<!-- To tell IE to use the latest rendering engine -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <!-- Theme is from http://bootswatch.com/flatly/ -->
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/styles.css">
